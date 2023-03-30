770126a6501726a754b7f02685aaef1501ab2b57
bukiz-cloud <85228475+bukiz-cloud@users.noreply.github.com>
2023-03-30 19:10:03 +0100

    updating the task model

--- model/Task.js
+++ model/Task.js
@@ -4,12 +4,12 @@ const taskSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
-  },
+      },
   description: {
     type: String,
     required: true,
   },
-  
+
   status: {
     type: String,
     enum: ['pending', 'in progress', 'completed'],
